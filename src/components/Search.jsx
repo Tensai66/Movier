fa2f2b76408b27a85aaf2806cf5499c72875ccd1
Tensai66 <mayur@khatri.cc>
2017-11-08 18:02:00 -0500

    Search bar suggestions based on input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -11,14 +11,13 @@ class Search extends Component {
 
   onChange = (e) => {
     this.setState({[e.target.name]: [e.target.value]}) // Changes the specified targets state value
-  }
-
-  searchMovies = (e) => {
-    e.preventDefault();
-
     const key = '471d212907833bde6a872c6b03ecbfdb';
 
-    fetch('https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=${this.state.search}&page=1&include_adult=false')
+    if(this.state.search == '') {
+      return;
+    }
+
+    fetch('https://api.themoviedb.org/3/search/movie?api_key=' + key + '&language=en-US&query=' + this.state.search + '&page=1&include_adult=false')
       .then(response => {
         if (response.status != 200) {
           console.log('Error: ' + response.status);
@@ -34,17 +33,48 @@ class Search extends Component {
         console.log('Fetch error :-S', err)
       })
   }
+
+  // searchMovies = (e) => {
+  //   e.preventDefault();
+
+  //   const key = '471d212907833bde6a872c6b03ecbfdb';
+
+  //   fetch('https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=${this.state.search}&page=1&include_adult=false')
+  //     .then(response => {
+  //       if (response.status != 200) {
+  //         console.log('Error: ' + response.status);
+  //         return;
+  //       }
+  //       response.json().then(data => {
+  //         const results = data.results;
+  //         this.setState({ results });
+  //       });
+  //     })
+
+  //     .catch(err => {
+  //       console.log('Fetch error :-S', err)
+  //     })
+  // }
+
+  handleKeyDown = (e) => {
+    if (e.key == 'Enter') {
+      return this.handleSubmit(this.searchMovies);
+    }
+    console.log("worked");
+  }
+  
   render() {
     return (
         <div className="searchBarContainer">
-          <form className="searchContent">
+          <form onSubmit={this.handleSubmit} className="searchContent">
             <input
               type="text"
               onChange={this.onChange}
               value={this.state.search}
               className="searchBar"
               placeholder="Search a movie title..."
-              name="search" />
+              name="search" required />
+            <SearchResults results={this.state.results} />
           </form>
         </div>
     );
--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -2,10 +2,32 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class SearchResults extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    
+  }
   render() {
     const imageLink = 'https://image.tmdb.org/t/p/w300';
     return (
-      <div></div>
+      <ul id="results" onClick={this.handleClick}>
+        {this.props.results.map((element, index) => {
+          return(
+            <li key={index} onClick={this.handleClick}>
+              <Link to={`/movie/${this.props.results[index].id}`} >
+                <img src={this.props.results[index].poster_path === null ? 'http://via.placeholder.com/640x960' : `${imageLink}${this.props.results[index].poster_path}`} alt={`${this.props.results[index].title} poster`} className="resultPoster" />
+                <div>
+                  <p>{this.props.results[index].title}</p>
+                  <p>{this.props.results[index].release_date}</p>
+                </div>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
     );
   }
 }
