1c0e883054bf85acfcc3f28b3b4fcdd57e16e3d4
Tensai66 <mayur@khatri.cc>
2017-11-15 16:00:39 -0500

    added movie page with movie img/genre/rating/description

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,11 +1,77 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar.jsx';
+import Search from './Search.jsx';
 
 class Movie extends Component {
+  state = {
+    movie: {
+      genres: []
+    }
+  }
+
+  getData = () => {
+    const key = '471d212907833bde6a872c6b03ecbfdb';
+    const movieId = window.location.pathname.substring(7);
+
+    fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${key}&language=en-US&append_to_response=credits`)
+    .then(response => {
+      if (response.status !== 200) {
+        console.log('Error: ' + response.status);
+        return;
+      }
+
+      response.json().then(data => {
+        const movie = data;
+        this.setState({ movie });
+      });
+
+    })
+    .catch(err => {
+      console.log('Fetch Error :-S', err);
+    })  
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.movie !== this.state.movie) {
+      this.getData();
+    }
+  }
+
   render() {
     return (
-        <div>
-          
+      <div className="movieContainer">
+        <Navbar />
+        <Search className="searchContent" />
+
+        <div className="movieContent">
+          <div className="moviePoster">
+            <img src={this.state.movie.poster_path === null ? 'http://via.placeholder.com/640x960' : `https://image.tmdb.org/t/p/w640${this.state.movie.poster_path}`} alt={`${this.state.movie.title} poster`} className="movieImg" />
+          </div>
+
+          <section className="movieDetails">
+            <h2 className="movieTitle">{this.state.movie.title}</h2>
+            <ul className="movieDetails">
+              <li><span>Release date:</span> {this.state.movie.release_date}</li>
+              <li><span>Rating:</span> {this.state.movie.vote_average}</li>
+
+              <li><span>Genres: </span> {this.state.movie.genres.map((element, index) => {
+                  if (index < this.state.movie.genres.length - 1) {
+                    return this.state.movie.genres[index].name + ', '
+                  } else {
+                    return this.state.movie.genres[index].name
+                  }
+                })}
+              </li>
+              
+            </ul>
+            <p>{this.state.movie.overview}</p>
+          </section>
         </div>
+      </div>
     );
   }
 }
